e1c3676263f59793d0e6f24c8f400ac56f82f427
Nkiriobasi <obasinkiri@gmail.com>
2022-07-19 22:03:19 +0100

    added a button component

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Button = () => {
+  return (
+    <button className="bg-red-400 hover:bg-red-300 text-white font-bold py-3.5 px-8 rounded-full text-xs">Get Started</button>
+  );
+}
+
+export default Button;
